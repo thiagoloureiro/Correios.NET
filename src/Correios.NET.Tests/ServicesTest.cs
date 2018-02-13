afdf5fefeab94ec21becd526e7eeb61ca1009a9f
Thiago Loureiro de Azevedo <thiago.loureiro.de.azevedo@playngo.com>
2018-02-13 18:43:18 +0100

    Updated Code

--- src/Correios.NET.Tests/ServicesTest.cs
+++ src/Correios.NET.Tests/ServicesTest.cs
@@ -4,80 +4,78 @@ using Xunit;
 
 namespace Correios.NET.Tests
 {
-    public class ServicesTest
-    {
-        private readonly string _packageHtml;
-        private readonly string _packageDeliveredHtml;
-        private readonly string _addressHtml;
+	public class ServicesTest
+	{
+		private readonly string _packageHtml;
+		private readonly string _packageDeliveredHtml;
+		private readonly string _addressHtml;
 
-        public ServicesTest()
-        {
-            _packageHtml = ResourcesReader.GetResourceAsString("Pacote.html");
-            _packageDeliveredHtml = ResourcesReader.GetResourceAsString("PacoteEntregue.html");
-            _addressHtml = ResourcesReader.GetResourceAsString("Endereco.html");
-        }
+		public ServicesTest()
+		{
+			_packageHtml = ResourcesReader.GetResourceAsString("Pacote.html");
+			_packageDeliveredHtml = ResourcesReader.GetResourceAsString("PacoteEntregue.html");
+			_addressHtml = ResourcesReader.GetResourceAsString("Endereco.html");
+		}
 
-        [Fact]
-        public void PackageTrackingService_Live_ShouldReturnCodeAndStatuses()
-        {
-            const string packageCode = "DU713842539BR";
-            IServices services = new Services();
-            var result = services.GetPackageTracking(packageCode);
+		[Fact]
+		public void PackageTrackingService_Live_ShouldReturnCodeAndStatuses()
+		{
+			const string packageCode = "BE051376688BR";
+			IServices services = new Services();
+			var result = services.GetPackageTracking(packageCode);
 
-            result.Code.Should().Be(packageCode);
-            result.TrackingHistory.Count.Should().BeGreaterThan(0);
-        }
+			result.Code.Should().Be(packageCode);
+			result.TrackingHistory.Count.Should().BeGreaterThan(0);
+		}
 
-        [Fact]
-        public void AddressService_Live_ShouldReturnAddress()
-        {
-            const string zipCode = "11441080";
-            IServices services = new Services();
-            var result = services.GetAddress(zipCode);
+		[Fact]
+		public void AddressService_Live_ShouldReturnAddress()
+		{
+			const string zipCode = "11441080";
+			IServices services = new Services();
+			var result = services.GetAddress(zipCode);
 
-            result.localidade.Should().Be("Guarujá");
-            result.uf.Should().Be("SP");
-        }
+			result.localidade.Should().Be("Guarujá");
+			result.uf.Should().Be("SP");
+		}
 
-        [Fact]
-        public void AddressService_Async_ShouldReturnAddress()
-        {
-            const string zipCode = "11441080";
-            IServices services = new Services();
-            var result = services.GetAddressAsync(zipCode).Result;
+		[Fact]
+		public void AddressService_Async_ShouldReturnAddress()
+		{
+			const string zipCode = "11441080";
+			IServices services = new Services();
+			var result = services.GetAddressAsync(zipCode).Result;
 
-            result.localidade.Should().Be("Guarujá");
-            result.uf.Should().Be("SP");
-        }
+			result.localidade.Should().Be("Guarujá");
+			result.uf.Should().Be("SP");
+		}
 
-        [Fact]
-        public void PackageTrackingService_ShouldReturnStatuses()
-        {
-            const string packageCode = "DU713842539BR";
-            var services = new Moq.Mock<IServices>();
-            services.Setup(s => s.GetPackageTracking(packageCode))
-                .Returns(Package.Parse(_packageHtml));
+		[Fact]
+		public void PackageTrackingService_ShouldReturnStatuses()
+		{
+			const string packageCode = "BE051376688BR";
+			var services = new Moq.Mock<IServices>();
+			services.Setup(s => s.GetPackageTracking(packageCode))
+				.Returns(Package.Parse(_packageHtml));
 
-            var result = services.Object.GetPackageTracking(packageCode);
+			var result = services.Object.GetPackageTracking(packageCode);
 
-            result.Code.Should().Be(packageCode);
-            result.TrackingHistory.Should().HaveCount(3);
-        }
+			result.Code.Should().Be(packageCode);
+			result.TrackingHistory.Should().HaveCount(3);
+		}
 
-        [Fact]
-        public void PackageTrackingService_ShouldBeDelivered()
-        {
-            const string packageCode = "DV248292626BR";
-            var services = new Moq.Mock<IServices>();
-            services.Setup(s => s.GetPackageTracking(packageCode))
-                .Returns(Package.Parse(_packageDeliveredHtml));
+		[Fact]
+		public void PackageTrackingService_ShouldBeDelivered()
+		{
+			const string packageCode = "BE051376688BR";
+			var services = new Moq.Mock<IServices>();
+			services.Setup(s => s.GetPackageTracking(packageCode))
+				.Returns(Package.Parse(_packageDeliveredHtml));
 
-            var result = services.Object.GetPackageTracking(packageCode);
+			var result = services.Object.GetPackageTracking(packageCode);
 
-            result.Code.Should().Be(packageCode);
-            result.IsDelivered.Should().BeTrue();
-        }
-
-       
-    }
+			result.Code.Should().Be(packageCode);
+			result.IsDelivered.Should().BeTrue();
+		}
+	}
 }
